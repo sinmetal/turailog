ba10b5cb85f1dcaa79101db44dd8a4073f9f07d9
sinmetal <sinmetal@sinmetalcraft.jp>
2021-02-15 17:39:19 +0900

    日によってschemaが変わるという謎の地獄

--- turaijson.go
+++ turaijson.go
@@ -29,6 +29,16 @@ type MetaData2 struct {
 	Tags    []string
 }
 
+type MetaData3 struct {
+	ID             string
+	Value          string
+	MetaData3Inner *MetaData3Inner
+}
+
+type MetaData3Inner struct {
+	Hello string
+}
+
 type TuraiJsonOutputter struct {
 	CurrentID   int64
 	SleepSecond int64
@@ -41,22 +51,9 @@ func (outputter *TuraiJsonOutputter) Run(ctx context.Context) {
 			fmt.Println("done:TuraiJsonOutputter.Run()", ctx.Err())
 			return
 		default:
-			metadata := make(map[string]interface{})
-			metadata["hoge"] = MetaData1{
-				ID:    uuid.NewString(),
-				Kind:  "TuraiKimoti",
-				Value: rand.Int63(),
-			}
-			metadata["fuga"] = MetaData2{
-				ID:      uuid.NewString(),
-				Content: uuid.NewString(),
-				Tags:    []string{"AppEngine", "ComputeEngine", "CloudSQL", "CloudStorage", "BigQuery"},
-			}
-			outputter.Log(&TuraiJson{
-				ID:         outputter.CurrentID,
-				Metadata:   metadata,
-				AppVersion: "v0.0.0",
-			})
+			now := time.Now()
+			category := now.Day() % 3
+			outputter.Log(outputter.GenerateTurai(category))
 			outputter.CurrentID++
 			time.Sleep(time.Duration(outputter.SleepSecond) * time.Second)
 			outputter.SleepSecond += 1 + outputter.SleepSecond
@@ -67,5 +64,37 @@ func (outputter *TuraiJsonOutputter) Run(ctx context.Context) {
 func (outputter *TuraiJsonOutputter) Log(value *TuraiJson) {
 	if err := json.NewEncoder(os.Stdout).Encode(value); err != nil {
 		fmt.Println(err.Error())
+	}
+}
+
+func (outputter *TuraiJsonOutputter) GenerateTurai(category int) *TuraiJson {
+	metadata := make(map[string]interface{})
+
+	switch category {
+	case 1:
+		metadata["hoge"] = MetaData3{
+			ID:    uuid.NewString(),
+			Value: "Hello Hoge",
+			MetaData3Inner: &MetaData3Inner{
+				Hello: "World",
+			},
+		}
+	default:
+		metadata["hoge"] = MetaData1{
+			ID:    uuid.NewString(),
+			Kind:  "TuraiKimoti",
+			Value: rand.Int63(),
+		}
+		metadata["fuga"] = MetaData2{
+			ID:      uuid.NewString(),
+			Content: uuid.NewString(),
+			Tags:    []string{"AppEngine", "ComputeEngine", "CloudSQL", "CloudStorage", "BigQuery"},
+		}
+	}
+
+	return &TuraiJson{
+		ID:         outputter.CurrentID,
+		Metadata:   metadata,
+		AppVersion: "v0.0.0",
 	}
 }
