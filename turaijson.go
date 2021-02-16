0cfc756ce4194ebdc50611df5e5236f7f1061ef4
sinmetal <sinmetal@sinmetalcraft.jp>
2021-02-16 19:56:29 +0900

    日毎にSchema変えていたが、GCSだと日は気にする必要がないことに気付いたので、UnitTimeにした
    Keyに色んな種類が入ってくるmapを追加

--- turaijson.go
+++ turaijson.go
@@ -39,6 +39,17 @@ type MetaData3Inner struct {
 	Hello string
 }
 
+type MetaData4 struct {
+	KV               map[string]int
+	LabelAnnotations []*LabelAnnotation
+}
+
+type LabelAnnotation struct {
+	ID          string
+	Description string
+	Score       float64
+}
+
 type TuraiJsonOutputter struct {
 	CurrentID   int64
 	SleepSecond int64
@@ -52,7 +63,7 @@ func (outputter *TuraiJsonOutputter) Run(ctx context.Context) {
 			return
 		default:
 			now := time.Now()
-			category := now.Day() % 3
+			category := int(now.Unix() % 3)
 			outputter.Log(outputter.GenerateTurai(category))
 			outputter.CurrentID++
 			time.Sleep(time.Duration(outputter.SleepSecond) * time.Second)
@@ -79,6 +90,16 @@ func (outputter *TuraiJsonOutputter) GenerateTurai(category int) *TuraiJson {
 				Hello: "World",
 			},
 		}
+	case 2:
+		kv := make(map[string]int)
+		for i := 0; i < 30; i++ {
+			kv[uuid.NewString()] = rand.Int()
+		}
+
+		metadata["KV"] = MetaData4{
+			KV:               kv,
+			LabelAnnotations: RandomLabelAnnotations(30),
+		}
 	default:
 		metadata["hoge"] = MetaData1{
 			ID:    uuid.NewString(),
@@ -96,5 +117,38 @@ func (outputter *TuraiJsonOutputter) GenerateTurai(category int) *TuraiJson {
 		ID:         outputter.CurrentID,
 		Metadata:   metadata,
 		AppVersion: "v0.0.0",
+	}
+}
+
+func RandomLabelAnnotations(count int) []*LabelAnnotation {
+	m := make(map[string]*LabelAnnotation)
+	for i := 0; i < count; i++ {
+		la := RandomLabelAnnotation()
+		m[la.ID] = la
+	}
+	var las []*LabelAnnotation
+	for _, v := range m {
+		las = append(las, v)
+	}
+	return las
+}
+
+func RandomLabelAnnotation() *LabelAnnotation {
+	i := rand.Int() % 10
+	switch i {
+	case 0:
+		return &LabelAnnotation{ID: "m001", Description: "dog"}
+	case 1:
+		return &LabelAnnotation{ID: "m002", Description: "cat"}
+	case 2:
+		return &LabelAnnotation{ID: "m003", Description: "bird"}
+	case 3:
+		return &LabelAnnotation{ID: "m004", Description: "fish"}
+	case 4:
+		return &LabelAnnotation{ID: "m005", Description: "shark"}
+	case 5:
+		return &LabelAnnotation{ID: "m006", Description: "phoenix"}
+	default:
+		return &LabelAnnotation{ID: uuid.NewString(), Description: "random"}
 	}
 }
